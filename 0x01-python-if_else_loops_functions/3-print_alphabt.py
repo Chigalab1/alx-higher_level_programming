8515ee55dcd9d6173fd4c3fbddee796fefb8ea1d
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-06-06 14:28:28 -0700

    two figits

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for my_l in range(97, 123):
-    if chr(my_l) is not 'q' and chr(my_l) is not 'e':
+    if chr(my_l) != 'q' and chr(my_l) != 'e':
         print("{}".format(chr(my_l)), end="")
+
