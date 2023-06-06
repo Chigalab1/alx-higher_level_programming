a58e02dbb823088dae578122773a16935cd6616e
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-06-06 13:14:09 -0700

    letter in lower case

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 2-print_alphabet.py
+
+"""Program that print the alphabet in lowercase, not followed by a new line."""
+for l in range(97, 123):
+    print("{}".format(chr(l)), end="")
