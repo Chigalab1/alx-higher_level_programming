fcfe4b9e20bb31f236bc8b97692f890fb088431f
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-06-06 14:43:49 -0700

    0int tepalpha

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 100-print_tebahpla.py
+""""func that Print the alphabet in reverse order alternating upper- and lower-case."""
+i = 0
+for c in range(var('z'), var('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 101-remove_char_at.py
+def remove_char_at(str, n):
+    """func that Create a copy of the string without the character at position n."""
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
