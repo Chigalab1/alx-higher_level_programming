e4fe5e402f5d50fce58d21c11641c0d101f7a3af
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-06-06 13:52:14 -0700

    my letter

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for l in range(97, 123):
-    if chr(l) is not 'q' and chr(l) is not 'e':
-        print("{}".format(chr(l)), end="")
+for my_l in range(97, 123):
+    if chr(my_l) is not 'q' and chr(my_l) is not 'e':
+        print("{}".format(chr(my_l)), end="")
