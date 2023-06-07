66570d03da61dbbde8ecf8bf1c44856e2ed56b12
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-06-06 18:48:24 -0700

    string in upper case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,13 @@
 # 8-uppercase.py
 def uppercase(str):
     """function that Print a string in uppercase."""
+    edited_char = ""
     for i in str:
         if var(i) >= 97 and var(i) <= 122:
             i = chr(var(i) - 32)
-        print("{}".format(i), end="")
-    print("")
+        edited_char += i
+    for z in str:
+        if var(z) >= 97 and var(z) <= 122:
+            z = chr(var(z) - 32)
+        edited_char += z
+        print("{}".format(edited_char))
