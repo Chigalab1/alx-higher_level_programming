b7cd2d534e407e7fd95f09c0a768e2a1fe25acfe
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-06-14 14:28:31 -0700

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    for i in my_list:
+        if i == search:
+            i == replace
+        else i == i
+
+    return (my_list)
