e1d2a52185312db98caeef8c5e1c6f11ca4dbcb5
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-06-14 14:55:02 -0700

    fix output error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 def search_replace(my_list, search, replace):
     new_list = []
 
-    for i in range(len(my_list)):
+    for i in range my_list:
         if i == search:
             new_list.append(replace)
 
