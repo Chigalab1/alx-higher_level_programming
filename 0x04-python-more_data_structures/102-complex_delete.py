d31eca74cb13f60cf875c576d80dbda3ce14a86d
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-06-14 15:24:35 -0700

    complex delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    my_keys = list(a_dictionary.keys())
+
+    for dic in my_keys:
+        if dic == a_dictionary.get(dic):
+            del a_dictionary[dic]
+
+    return (a_dictionary)
