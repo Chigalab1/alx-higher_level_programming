a682654b68dd32288b5041b71703bb94595bbf8b
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-06-26 09:56:02 -0700

    prints elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """ function that prints x elements
+    of a list """
+
+    r = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            r += 1
+        except IndexError:
+            break
+        print("")
+        return (r)
