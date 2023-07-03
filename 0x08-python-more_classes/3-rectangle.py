e28cbdb45528618bd3b00a6583cf70377105576e
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-07-03 16:10:08 -0700

    2-rect

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-""" defines a rectangle """
+"""defines a rectangle
+"""
 
 
 class Rectangle:
     """ class rectangle"""
-
     def __init__(self, width=0, height=0):
-        """ initialize the rectangle"""
+        """ Initialize the rectangle"""
         self.width = width
-        self.heig = height
+        self.height = height
 
     @property
     def width(self):
-        """width of rectangle"""
+        """ width of rectangle
+        """
         return self.__width
 
     @property
     def height(self):
-        """height of rectangle"""
+        """ height of rectangle
+        """
         return self.__height
 
     @width.setter
     def width(self, value):
-        """set width
+        """ set width
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -32,31 +34,28 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """set height
+        """ set height
         """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >=0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """returns the area of
-        a rectangle"""
+        """ returns area of rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
-        """returns the perimeter of a rectangle
-        """
+        """ returns perimeter of rectangle"""
         if self.__width is 0 or self.__height is 0:
             return 0
         return self.__width * 2 + self.__height * 2
 
     def __str__(self):
-        """ return the rectangle of character #
+        """ return the rectangle with the character #
         """
         if self.__width is 0 or self.__height is 0:
             return ""
-
         return ("\n".join(["".join(["#" for i in range(self.__width)])
                 for j in range(self.__height)]))
