1ac4c2c00acde4ca5d557e264226be68229b3de5
Chigalab1 <Chigozirimuwaezuoke@gmail.com>
2023-09-12 21:34:06 +0000

    concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const fir = require('fir').promises;
+const fs = require('fs').promises;
 const { argv } = require('process');
 
-fir.readFile(argv[2], 'utf8')
-  .then(data => fir.writeFile(argv[4], data, 'utf8'))
+fs.readFile(argv[2], 'utf8')
+  .then(data => fs.writeFile(argv[4], data, 'utf8'))
   .catch(err => console.error(err));
 
-fir.readFile(argv[3], 'utf8')
-  .then(data => fir.writeFile(argv[4], data, { flag: 'a' }, 'utf8'))
+fs.readFile(argv[3], 'utf8')
+  .then(data => fs.writeFile(argv[4], data, { flag: 'a' }, 'utf8'))
   .catch(err => console.error(err));
