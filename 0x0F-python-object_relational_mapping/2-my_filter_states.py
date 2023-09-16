8ffebd5352b9d0c71c014d54a37554cc31d0367f
Chigalab1 <Chigozirimuwaezuoke@gmail.com>
2023-09-16 20:47:32 +0000

    filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """
-    A script that lists all states with a name starting with
-    N (upper N) from the database hbtn_0e_0_usa
+    A script that takes in an argument and displays all values in the
+    states table of hbtn_0e_0_usa where name matches the argument.
 """
 
 
@@ -19,9 +19,11 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states\
-                    WHERE name LIKE BINARY 'N%'\
-                    ORDER BY id ASC")
+    sql = """ SELECT * FROM states
+          WHERE name LIKE BINARY '{}'
+          ORDER BY id ASC """.format(sys.argv[4])
+
+    cursor.execute(sql)
 
     data = cursor.fetchall()
 
